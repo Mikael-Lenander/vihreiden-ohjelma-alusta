efa952d51a745a47130cf70f5bc2269831cf7431
Mikael Lenander <mikael.lenander1@gmail.com>
2024-07-31 00:45:36 +0300

    Simple title-based search feature

--- browser/vihreat-ohjelmat/src/components/ProgramBadge.tsx
+++ browser/vihreat-ohjelmat/src/components/ProgramBadge.tsx
@@ -6,28 +6,35 @@ import { Program, ontology as vihreat, useStatusInfo } from 'vihreat-lib';
 interface ProgramBadgeProps {
   index: number;
   collection: Collection;
+  searchQuery: string;
 }
 
 export function ProgramBadge({
   index,
   collection,
+  searchQuery,
 }: ProgramBadgeProps): JSX.Element {
   const program = useMemberFromCollection<Program>(collection, index);
   const [title] = useString(program, vihreat.properties.title);
   const [subtitle] = useString(program, vihreat.properties.subtitle);
   const id = program.subject.split('/').pop();
   const status = useStatusInfo(program);
+  const searchHit =
+    title?.toLowerCase().includes(searchQuery) ||
+    subtitle?.toLowerCase().includes(searchQuery);
 
   return (
     <>
-      <NavLink to={`/ohjelmat/${id}`} className={linkStyling}>
-        <p className={`vo-programbadge vo-programbadge-${status.color}`}>
-          <p className='vo-programbadge-subtitle'>{subtitle}</p>
-          <p className='vo-programbadge-title' title={title}>
-            {title}
+      {searchHit && (
+        <NavLink to={`/ohjelmat/${id}`} className={linkStyling}>
+          <p className={`vo-programbadge vo-programbadge-${status.color}`}>
+            <p className='vo-programbadge-subtitle'>{subtitle}</p>
+            <p className='vo-programbadge-title' title={title}>
+              {title}
+            </p>
           </p>
-        </p>
-      </NavLink>
+        </NavLink>
+      )}
     </>
   );
 }
