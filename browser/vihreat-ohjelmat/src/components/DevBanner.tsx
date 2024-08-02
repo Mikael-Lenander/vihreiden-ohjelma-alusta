01dacd20a37d2c88d251cbae48d78589ee2d88d2
Joonas Lipping <lippinj@outlook.com>
2024-08-02 09:05:27 +0300

    Refine dev banner

---
+++ browser/vihreat-ohjelmat/src/components/DevBanner.tsx
@@ -0,0 +1,7 @@
+export function DevBanner(): JSX.Element {
+  return (
+    <p className='vo-program-dev-banner'>
+      ⚠ Sivusto on kehitysvaiheessa eikä sen sisältö välttämättä vastaa Vihreiden virallisia ohjelmia.
+    </p>
+  );
+}
