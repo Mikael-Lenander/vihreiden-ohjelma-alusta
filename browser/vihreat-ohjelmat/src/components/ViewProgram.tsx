631cffdc991a4e8a95e04bd15182ac47da35d829
Joonas Lipping <lippinj@outlook.com>
2024-08-21 18:21:38 +0300

    Reorganize

--- browser/vihreat-ohjelmat/src/components/ViewProgram.tsx
+++ browser/vihreat-ohjelmat/src/components/ViewProgram.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
 import { useResource } from '@tomic/react';
 import { ProgramView, Program } from 'vihreat-lib';
 
@@ -9,10 +9,27 @@ export function ViewProgram(): JSX.Element {
   const resource = useResource<Program>(subject);
 
   if (resource === undefined) {
-    <p>Failed to load resource {subject}. Is the server running?</p>;
+    return (
+      <>
+        <BackButton />
+        <p>Failed to load resource {subject}. Is the server running?</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <BackButton />
+        <ProgramView resource={resource} />;
+      </>
+    );
   }
-
-  return <ProgramView resource={resource} />;
 }
-
 export default ViewProgram;
+
+function BackButton(): JSX.Element {
+  return (
+    <NavLink to='/' id='vo-back-button'>
+      <span>&#x2190; ohjelmien etusivulle</span>
+    </NavLink>
+  );
+}
