85a419aa02726ec92b683436ea1eac3a4dbe914d
Joonas Lipping <lippinj@outlook.com>
2024-09-24 13:21:33 +0300

    Placate lint

--- vihreat-ohjelmat/src/hooks.ts
+++ vihreat-ohjelmat/src/hooks.ts
@@ -1,7 +1,7 @@
 import { useArray, core, Resource } from '@tomic/react';
 import { ontology } from './ontologies/ontology';
 
-export function useProgramClass(resource: Resource<any>) {
+export function useProgramClass(resource: Resource) {
   const [classes] = useArray(resource, core.properties.isA);
   const programClass = classes.find(c => c !== ontology.classes.program);
 
